ead1890bbb3d6af2ffa1e2c9826cc397c7927a5c
Handwoong <handwoong@gmail.com>
2023-11-11 05:45:22 +0900

    feat(config): jpa auditing 사용 설정

--- src/main/java/com/handwoong/rainbowletter/RainbowLetterApplication.java
+++ src/main/java/com/handwoong/rainbowletter/RainbowLetterApplication.java
@@ -2,7 +2,9 @@ package com.handwoong.rainbowletter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class RainbowLetterApplication {
     public static void main(final String[] args) {
