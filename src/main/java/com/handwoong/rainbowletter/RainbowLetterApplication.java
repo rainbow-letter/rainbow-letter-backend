81b3f31fd904a38aa8d123742784e2b73eec39d1
Handwoong <handwoong@gmail.com>
2023-11-11 04:56:23 +0900

    style(application): 빈줄 컨벤션 수정

--- src/main/java/com/handwoong/rainbowletter/RainbowLetterApplication.java
+++ src/main/java/com/handwoong/rainbowletter/RainbowLetterApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RainbowLetterApplication {
-
     public static void main(final String[] args) {
         SpringApplication.run(RainbowLetterApplication.class, args);
     }
-
 }
