2c400541a88dc709f284fdd71452a3510f63c708
Handwoong <handwoong@gmail.com>
2023-11-11 05:41:23 +0900

    feat(config): p6spy sql 포매터 구현

---
+++ src/main/java/com/handwoong/rainbowletter/config/p6spy/FormatType.java
@@ -0,0 +1,20 @@
+package com.handwoong.rainbowletter.config.p6spy;
+
+import java.util.List;
+
+public enum FormatType {
+    DDL(List.of("create", "alter", "comment"));
+
+    private final List<String> commands;
+
+    FormatType(final List<String> commands) {
+        this.commands = commands;
+    }
+
+    public static boolean isDDL(final String sql) {
+        return FormatType.DDL
+                .commands
+                .stream()
+                .anyMatch(sql::startsWith);
+    }
+}
