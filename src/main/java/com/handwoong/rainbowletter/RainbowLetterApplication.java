d332313bdd245ddc1307f704d7e4c2db49d1a2b9
Handwoong <handwoong@gmail.com>
2023-11-11 04:38:34 +0900

    :tada: initialize project

---
+++ src/main/java/com/handwoong/rainbowletter/RainbowLetterApplication.java
@@ -0,0 +1,13 @@
+package com.handwoong.rainbowletter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RainbowLetterApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(RainbowLetterApplication.class, args);
+    }
+
+}
