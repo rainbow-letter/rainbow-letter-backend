c3d998c17f365f974da8d2f5ef2cc76db2490c5d
Handwoong <handwoong@gmail.com>
2023-11-11 05:56:22 +0900

    chore: 프로젝트 환경 설정 (#2)
    * refactor(application): contextLoad 테스트 삭제
    
    * style(application): 빈줄 컨벤션 수정
    
    * feat(config): p6spy sql 포매터 구현
    
    * feat(config): querydsl 사용 설정
    
    * feat(config): jpa auditing 사용 설정

--- src/main/java/com/handwoong/rainbowletter/RainbowLetterApplication.java
+++ src/main/java/com/handwoong/rainbowletter/RainbowLetterApplication.java
@@ -2,12 +2,12 @@ package com.handwoong.rainbowletter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class RainbowLetterApplication {
-
     public static void main(final String[] args) {
         SpringApplication.run(RainbowLetterApplication.class, args);
     }
-
 }
