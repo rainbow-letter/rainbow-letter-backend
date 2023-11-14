d4d7060ca15fe968b57195f3744c4d9cae733a91
Handwoong <handwoong@gmail.com>
2023-11-15 06:29:10 +0900

    feat(exception): 예외 처리 핸들러 구현 (#7)

---
+++ src/main/java/com/handwoong/rainbowletter/exception/ApiExceptionHandler.java
@@ -0,0 +1,113 @@
+package com.handwoong.rainbowletter.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestControllerAdvice
+public class ApiExceptionHandler {
+    private static final String MESSAGE_FORMAT = "[%s] %s %s";
+    private static final String VALIDATION_MESSAGE_FORMAT = "\n%s-[%s]-[%s]";
+
+    @ExceptionHandler({RainbowLetterException.class})
+    public ResponseEntity<ErrorResponse> rainbowLetter(final RainbowLetterException exception) {
+        final ErrorCode errorCode = exception.getErrorCode();
+        logWarn(errorCode, exception, exception.getField());
+        return createErrorResponse(errorCode);
+    }
+
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    public ResponseEntity<ErrorResponse> methodArgumentNotValid(final MethodArgumentNotValidException exception) {
+        final ErrorCode errorCode = ErrorCode.METHOD_ARGUMENT_NOT_VALID;
+        final String logMessage = createValidationLogMessage(exception);
+        final String errorMessage = createValidationErrorMessage(exception);
+        logWarn(errorCode, exception, logMessage);
+        return createErrorResponse(errorCode, errorMessage);
+    }
+
+    @ExceptionHandler({BadCredentialsException.class})
+    public ResponseEntity<ErrorResponse> badCredentials(final BadCredentialsException exception) {
+        final ErrorCode errorCode = ErrorCode.CHECK_EMAIL_AND_PASSWORD;
+        logWarn(errorCode, exception);
+        return createErrorResponse(errorCode);
+    }
+
+    @ExceptionHandler({UsernameNotFoundException.class})
+    public ResponseEntity<ErrorResponse> usernameNotFound(final UsernameNotFoundException exception) {
+        final ErrorCode errorCode = ErrorCode.IN_VALID_EMAIL;
+        logWarn(errorCode, exception, exception.getMessage());
+        return createErrorResponse(errorCode);
+    }
+
+    @ExceptionHandler({HttpRequestMethodNotSupportedException.class})
+    public ResponseEntity<ErrorResponse> methodNotSupported(final HttpRequestMethodNotSupportedException exception) {
+        final ErrorCode errorCode = ErrorCode.NOT_SUPPORT_METHOD;
+        logWarn(errorCode, exception);
+        return createErrorResponse(errorCode);
+    }
+
+    @ExceptionHandler({Exception.class})
+    public ResponseEntity<ErrorResponse> exception(final Exception exception) {
+        final ErrorCode errorCode = ErrorCode.SERVER_ERROR;
+        logError(errorCode, exception);
+        return createErrorResponse(errorCode);
+    }
+
+    private ResponseEntity<ErrorResponse> createErrorResponse(final ErrorCode errorCode) {
+        final ErrorResponse errorResponse = ErrorResponse.from(errorCode);
+        return new ResponseEntity<>(errorResponse, errorCode.getStatus());
+    }
+
+    private ResponseEntity<ErrorResponse> createErrorResponse(final ErrorCode errorCode, final String errorMessage) {
+        final ErrorResponse errorResponse = ErrorResponse.of(errorCode, errorMessage);
+        return new ResponseEntity<>(errorResponse, errorCode.getStatus());
+    }
+
+    private String createValidationErrorMessage(final MethodArgumentNotValidException exception) {
+        return exception.getBindingResult()
+                .getAllErrors()
+                .get(0)
+                .getDefaultMessage();
+    }
+
+    private String createValidationLogMessage(final MethodArgumentNotValidException exception) {
+        final StringBuilder sb = new StringBuilder();
+        final BindingResult bindingResult = exception.getBindingResult();
+        for (final FieldError fieldError : bindingResult.getFieldErrors()) {
+            final String message = String.format(
+                    VALIDATION_MESSAGE_FORMAT,
+                    fieldError.getDefaultMessage(),
+                    fieldError.getField(),
+                    fieldError.getRejectedValue());
+            sb.append(message);
+        }
+        return sb.toString();
+    }
+
+    private void logWarn(final ErrorCode errorCode, final Exception exception) {
+        final String logMessage = String.format(
+                MESSAGE_FORMAT, errorCode.name(), errorCode.getStatus(), errorCode.getMessage());
+        log.warn(logMessage, exception);
+    }
+
+    private void logWarn(final ErrorCode errorCode, final Exception exception, final String field) {
+        final String logMessage = String.format(
+                MESSAGE_FORMAT, errorCode.name(), errorCode.getStatus(), errorCode.getMessage() + "-" + field);
+        log.warn(logMessage, exception);
+    }
+
+    private void logError(final ErrorCode errorCode, final Exception exception) {
+        final String logMessage = String.format(
+                MESSAGE_FORMAT, errorCode.name(), errorCode.getStatus(), errorCode.getMessage());
+        log.error(logMessage, exception);
+    }
+}
