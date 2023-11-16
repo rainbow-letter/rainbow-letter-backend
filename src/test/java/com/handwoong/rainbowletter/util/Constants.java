45d911582d988373426d5358f33a96324c3d8cac
Handwoong <handwoong@gmail.com>
2023-11-17 00:37:38 +0900

    test(member): 회원가입 및 로그인 컨트롤러 테스트 (#13)
    * chore: 테스트용 h2 데이터베이스 의존성 추가
    
    * chore: 테스트용 rest-assured 의존성 추가
    
    * test(member): 회원가입 및 로그인 테스트 추가 (#10)

---
+++ src/test/java/com/handwoong/rainbowletter/util/Constants.java
@@ -0,0 +1,13 @@
+package com.handwoong.rainbowletter.util;
+
+public class Constants {
+    public static final String USER_EMAIL = "user@email.com";
+    public static final String USER_PASSWORD = "user1234";
+    public static final String ADMIN_EMAIL = "admin@email.com";
+    public static final String ADMIN_PASSWORD = "admin1234";
+    public static final String NEW_EMAIL = "handwoong@gmail.com";
+    public static final String NEW_PASSWORD = "password1";
+
+    private Constants() {
+    }
+}
