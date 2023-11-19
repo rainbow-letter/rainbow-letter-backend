e32e4165204d7acc0d8fc388f7a8dc62b5d18dbf
Handwoong <handwoong@gmail.com>
2023-11-19 20:34:13 +0900

    test(member): 회원 컨트롤러 테스트 추가 (#25)

--- src/test/java/com/handwoong/rainbowletter/controller/member/MemberControllerTest.java
+++ src/test/java/com/handwoong/rainbowletter/controller/member/MemberControllerTest.java
@@ -1,7 +1,11 @@
 package com.handwoong.rainbowletter.controller.member;
 
+import static com.handwoong.rainbowletter.config.security.JwtTokenAuthenticationFilter.AUTHORIZATION_HEADER_KEY;
+import static com.handwoong.rainbowletter.config.security.JwtTokenAuthenticationFilter.AUTHORIZATION_HEADER_TYPE;
 import static com.handwoong.rainbowletter.util.Constants.NEW_EMAIL;
 import static com.handwoong.rainbowletter.util.Constants.NEW_PASSWORD;
+import static com.handwoong.rainbowletter.util.Constants.USER_EMAIL;
+import static com.handwoong.rainbowletter.util.Constants.USER_PASSWORD;
 import static com.handwoong.rainbowletter.util.RestDocsUtils.getSpecification;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -59,13 +63,36 @@ public class MemberControllerTest extends ControllerTestProvider {
     }
 
     @Test
+    @DisplayName("중복된 이메일이 존재하면 400 예외가 발생한다.")
+    void duplicate_register_member() {
+        // given
+        final MemberRegisterRequest registerRequest = new MemberRegisterRequest(USER_EMAIL, USER_PASSWORD);
+
+        // when
+        final ExtractableResponse<Response> registerResponse = register(registerRequest);
+
+        // then
+        assertThat(registerResponse.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+
+    @Test
+    @DisplayName("로그인에 성공한 사용자가 회원가입 시도 시 403 예외가 발생한다.")
+    void already_login_member_register() {
+        // given
+        final MemberRegisterRequest registerRequest = new MemberRegisterRequest(NEW_EMAIL, NEW_PASSWORD);
+
+        // when
+        final ExtractableResponse<Response> registerResponse = register(registerRequest, userAccessToken);
+
+        // then
+        assertThat(registerResponse.statusCode()).isEqualTo(HttpStatus.FORBIDDEN.value());
+    }
+
+    @Test
     @DisplayName("유효한 로그인 요청이 들어오면 토큰을 발급한다.")
     void login() {
         // given
-        final MemberRegisterRequest registerRequest = new MemberRegisterRequest(NEW_EMAIL, NEW_PASSWORD);
-        register(registerRequest);
-
-        final MemberLoginRequest loginRequest = new MemberLoginRequest(NEW_EMAIL, NEW_PASSWORD);
+        final MemberLoginRequest loginRequest = new MemberLoginRequest(USER_EMAIL, USER_PASSWORD);
 
         // when
         final ExtractableResponse<Response> loginResponse = login(loginRequest);
@@ -75,5 +102,67 @@ public class MemberControllerTest extends ControllerTestProvider {
         assertThat(loginResponse.statusCode()).isEqualTo(HttpStatus.OK.value());
         assertThat(tokenResponse.grantType()).isEqualTo(GrantType.BEARER.getName());
         assertThat(tokenResponse.token()).isInstanceOf(String.class);
+    }
+
+    @Test
+    @DisplayName("이메일 인증을 완료하지 않은 사용자가 로그인 시도 시 401 예외가 발생한다.")
+    void not_verify_email_member_login() {
+        // given
+        final MemberRegisterRequest registerRequest = new MemberRegisterRequest(NEW_EMAIL, NEW_PASSWORD);
+        register(registerRequest);
+
+        final MemberLoginRequest loginRequest = new MemberLoginRequest(NEW_EMAIL, NEW_PASSWORD);
+
+        // when
+        final ExtractableResponse<Response> loginResponse = login(loginRequest);
+
+        // then
+        assertThat(loginResponse.statusCode()).isEqualTo(HttpStatus.UNAUTHORIZED.value());
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 사용자 이메일로 로그인 시도 시 400 예외가 발생한다.")
+    void login_notfound_member() {
+        // given
+        final MemberLoginRequest loginRequest = new MemberLoginRequest(NEW_EMAIL, NEW_PASSWORD);
+
+        // when
+        final ExtractableResponse<Response> loginResponse = login(loginRequest);
+
+        // then
+        assertThat(loginResponse.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+
+    @Test
+    @DisplayName("로그인에 성공한 사용자가 로그인 시도 시 403 예외가 발생한다.")
+    void already_login_member_login() {
+        // given
+        final MemberLoginRequest loginRequest = new MemberLoginRequest(USER_EMAIL, USER_PASSWORD);
+
+        // when
+        final ExtractableResponse<Response> loginResponse = login(loginRequest, userAccessToken);
+
+        // then
+        assertThat(loginResponse.statusCode()).isEqualTo(HttpStatus.FORBIDDEN.value());
+    }
+
+    private ExtractableResponse<Response> register(final MemberRegisterRequest registerRequest, final String token) {
+        return RestAssured
+                .given(getSpecification()).log().all()
+                .header(AUTHORIZATION_HEADER_KEY, AUTHORIZATION_HEADER_TYPE + " " + token)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(registerRequest)
+                .when().post("/api/members")
+                .then().log().all().extract();
+    }
+
+    private ExtractableResponse<Response> login(final MemberLoginRequest loginRequest, final String token) {
+        return RestAssured
+                .given(getSpecification()).log().all()
+                .header(AUTHORIZATION_HEADER_KEY, AUTHORIZATION_HEADER_TYPE + " " + token)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(loginRequest)
+                .when().post("/api/members/login")
+                .then().log().all().extract();
     }
 }
