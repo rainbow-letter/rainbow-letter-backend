9f4f6c620a6991741a80b7dbb33ed36671970b5b
Handwoong <handwoong@gmail.com>
2024-01-04 19:24:07 +0900

    feat(letter): 편지 예외 핸들러 구현

---
+++ src/main/java/com/handwoong/rainbowletter/letter/exception/LetterExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.handwoong.rainbowletter.letter.exception;
+
+import com.handwoong.rainbowletter.common.exception.BaseExceptionHandler;
+import com.handwoong.rainbowletter.common.exception.ErrorCode;
+import com.handwoong.rainbowletter.common.exception.ErrorResponse;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class LetterExceptionHandler extends BaseExceptionHandler {
+    @ExceptionHandler({SummaryFormatNotValidException.class})
+    public ResponseEntity<ErrorResponse> summaryFormatNotValid(final SummaryFormatNotValidException exception) {
+        final ErrorCode errorCode = exception.getErrorCode();
+        logWarn(errorCode, exception.getSummary());
+        return createErrorResponse(errorCode);
+    }
+
+    @ExceptionHandler({ContentFormatNotValidException.class})
+    public ResponseEntity<ErrorResponse> contentFormatNotValid(final ContentFormatNotValidException exception) {
+        final ErrorCode errorCode = exception.getErrorCode();
+        logWarn(errorCode, exception.getContent());
+        return createErrorResponse(errorCode);
+    }
+}
