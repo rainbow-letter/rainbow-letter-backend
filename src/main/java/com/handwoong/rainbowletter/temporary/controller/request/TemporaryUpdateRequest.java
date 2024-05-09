8aa45d74b0784d628e2ab122e29b28df4ab1938f
Handwoong <handwoong@gmail.com>
2024-05-10 01:33:45 +0900

    fix: 임시 저장 편지 수정 기능 버그 수정

--- src/main/java/com/handwoong/rainbowletter/temporary/controller/request/TemporaryUpdateRequest.java
+++ src/main/java/com/handwoong/rainbowletter/temporary/controller/request/TemporaryUpdateRequest.java
@@ -2,10 +2,11 @@ package com.handwoong.rainbowletter.temporary.controller.request;
 
 import com.handwoong.rainbowletter.temporary.domain.dto.TemporaryUpdate;
 
-public record TemporaryUpdateRequest(Long petId, String content) {
+public record TemporaryUpdateRequest(Long id, Long petId, String content) {
 
     public TemporaryUpdate toDto() {
         return TemporaryUpdate.builder()
+                .id(id)
                 .petId(petId)
                 .content(content)
                 .build();
